89155919713d80c1103da543010a32c7340f54d2
Apua <Apua.A.Aa@gmail.com>
2018-05-24 22:52:50 +0800

    [lib] support auto login
    Goal: make all API of `CirrusApi` support auto login feature, but TAE
    doesn't have to copy/paste the statements, and dryrun validator must
    work as usual.
    That's to say, TAE just implement method below::
    
        def method(self, a, b, c, ...):
            ...
    
    And it will be known by Robot Framework as::
    
        def method(self, a, b, c, ..., credential=None):
            if credential:
                if self.user_name is None:
                    self.login(**credential)
                elif credential['user_name'] != self.user_name:
                    self.logout()
                    self.login(**credential)
                else:
                    pass
            ...
    
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    POC1: invoke Robot Framework dynamic library API -- `get_keyword_names`,
    `run_keyword`, `get_keyword_arguments`.
    With `get_keyword_arguments`, it fulfill the "dryrun" requirement,
    however, the arguments information provided by `run_keyword` is not
    enough to determine whether :arg:`credential` is used by a high-level
    keyword or not.
    
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    POC2: to decorate :cls:`CirrusApi`, there are three stages to do so:
    
    1. after instantiation -- define `__getattribute__` in :cls:`CirrusApi`
    
    2. before instantiation, after creating class -- create a decorator
       :func:`auto_login` to modify all methods on :cls:`CirrusApi`
    
    3. before creating class (i.e. during creating class) -- define a meta
       class :cls:`AutoLogin` to modify all methods while new :cls:`CirrusApi`
    
    The 1st and 3rd stages will effect all classes, thus only 2nd is ideal.
    
    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    POC3: it is necessary to keep arguments' information correctly for
    "dryrun" requirement, thus usual decorator implementation doesn't help::
    
        def dec(func):
            from functools import wraps
            @wraps(func)
            def func_(*a, **kw):
                ...
            return func_
    
    A possible way is meta programming that, refer to original code, and
    generate new code.
    
        def dec(func):
            import ast, inspect, textwrap
            tree = ast.parse(inspect.getsource(testwrap.dedent(func)))
            ...
            exec(compile(tree, '<string>', 'exec'))
            return locals()[func.__name__]

--- resources/cirrus/rest_api.py
+++ resources/cirrus/rest_api.py
@@ -59,17 +59,45 @@ class AtlasApi:
             self.logout()
 
 
+def auto_login(cls):
+    import ast, inspect, textwrap
+    cls_source = inspect.getsource(cls)
+    tree = ast.parse(cls_source)
+    class_def = tree.body[0]
+    for stmt in class_def.body:
+        if isinstance(stmt, ast.FunctionDef):
+            assert 'credential' not in getattr(cls, stmt.name).__code__.co_varnames, stmt.name
+            self_var = stmt.args.args[0].arg
+            ref = ast.parse(textwrap.dedent(f'''\
+                    def ref({self_var}, credential=None):
+                        if credential:
+                            if {self_var}.user_name is None:
+                                {self_var}.login(**credential)
+                            elif credential['user_name'] != {self_var}.user_name:
+                                {self_var}.logout()
+                                {self_var}.login(**credential)
+                            else:
+                                pass
+                    ''')).body[0]
+            stmt.args.args.append(ref.args.args[-1])
+            stmt.args.defaults.append(ref.args.defaults[-1])
+            stmt.body = ref.body + stmt.body
+    exec(compile(tree, '<string>', 'exec'))
+    return locals()[cls.__name__]
+
+
+@auto_login
 class CirrusApi:
-    def add_hardware(self, hw, credential=None):
+    def add_hardware(self, hw):
         return 1
 
-    def create_reservation(self, rsv, credential=None):
+    def create_reservation(self, rsv):
         return 2
 
-    def release_reservation(self, rsv_id, credential=None):
+    def release_reservation(self, rsv_id):
         pass
 
-    def detail_reservation(self, rsv_id, credential=None):
+    def detail_reservation(self, rsv_id):
         if rsv_id == 3:
             raise AssertionError('HTTP 404 NOT FOUND')
         return 'reservation data'
