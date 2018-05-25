ba7993befad20bd438a3ffb76a8a89c1e865e9da
Apua <Apua.A.Aa@gmail.com>
2018-05-25 19:32:43 +0800

    misc: fix RUNME.sh after collecting

--- RUNME.sh
+++ RUNME.sh
@@ -1,8 +1,10 @@
-pybot -L TRACE -b debug.log -P resources \
-      -v host:cirrus-demo.tw.rdlabs.hpecorp.net \
-      Cirrus_REST_no_dependent/Reservation.robot
+#! /bin/sh
 
-pybot -L TRACE -b debug.log -P resources \
+pybot -L TRACE -b debug.log -P . \
+      -v host:cirrus-demo.tw.rdlabs.hpecorp.net \
+      Reservation.robot
+
+pybot -L TRACE -b debug.log -P . \
       -v host:cirrus-demo.tw.rdlabs.hpecorp.net \
       --dryrun -r none -l none -o none \
-      Cirrus_REST_no_dependent/Reservation.robot
+      Reservation.robot
